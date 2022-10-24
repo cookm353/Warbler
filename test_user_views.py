274b60afc5cc7fbd7bf6b43eeeb7e5eac67b5e86
Matthew Cook <cookm353@gmail.com>
2022-10-24 16:28:17 -0400

    adding a little boilerplate

---
+++ test_user_views.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestUserViews(TestCase):
+    def setUp(self):
+        ...
+        
+    def tearDown(self):
+        ...
+        
+    def test_login(self):
+        ...
